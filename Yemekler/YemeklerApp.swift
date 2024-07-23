3cb57d4ba92467b97af0b2afdd1ae2547fe44a70
mustafatavasli <musstafatavasli@gmail.com>
2024-07-23 10:26:26 +0300

    Initial Commit

---
+++ Yemekler/YemeklerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemeklerApp.swift
+//  Yemekler
+//
+//  Created by Mustafa TAVASLI on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct YemeklerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
